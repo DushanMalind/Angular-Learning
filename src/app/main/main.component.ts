ccf6a395136086c44abe940bbbe9c6eb6f245702
DushanMalind <dushanmalinda010@gmail.com>
2024-03-04 16:06:34 +0530

    Content Route part

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-main',
+  template: '<h1>Main Content</h1>',
+  standalone: true,
+
+})
+
+export class MainComponent{
+
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-navbar',
+  template: '<h1>Nav Bar</h1>',
+  standalone: true,
+
+})
+
+export class NavbarComponent{
+title = 'My First Angular App';
+}
