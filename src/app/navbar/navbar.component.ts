c1f7400ac483459b2b3afdefa2f242d60d644318
DushanMalind <dushanmalinda010@gmail.com>
2024-03-04 17:01:10 +0530

    Component Update Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import {Component} from "@angular/core";
 
 
-@Component({
+/*@Component({
   selector: 'app-navbar',
   template: '<h1>Nav Bar</h1>',
   standalone: true,
   styles:[`h1{color:blue; font-size: 45px}h1:hover{color:red;cursor: pointer}`]
 
+})*/
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  standalone: true,
+  styleUrls: ['./navbar.component.css']
 })
 
 export class NavbarComponent{
 title = 'My First Angular App';
 }
+
