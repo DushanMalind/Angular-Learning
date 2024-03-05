30de7b463a234d7f0a92199034502f555601a4ce
DushanMalind <dushanmalinda010@gmail.com>
2024-03-05 12:30:42 +0530

    Routing Part Title Add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: '', 'title': "Home", component: HomeComponent },
+  { path: 'about', 'title': "About", component: AboutComponent }
 ];
