f467011d1248a5890f14b2e5490f0d1ff67733c5
DushanMalind <dushanmalinda010@gmail.com>
2024-03-05 12:40:47 +0530

    PageNotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: '', 'title': "Home", component: HomeComponent },
-  { path: 'about', 'title': "About", component: AboutComponent }
+  { path: 'about', 'title': "About", component: AboutComponent },
+  {path: '**', component: PageNotFoundComponent}
 ];
