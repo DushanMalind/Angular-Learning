e9920df641d76f6cb258fc0ec776dc665e4c847e
DushanMalind <dushanmalinda010@gmail.com>
2024-03-06 18:14:43 +0530

    Button Click Data Fill Ok

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,20 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  @ViewChild('postTitleElement') postTitleElement: ElementRef | undefined;
+  @ViewChild('postContentElement') postContentElement: ElementRef | undefined;
+  @ViewChild('imageUrlElement') imageUrlElement: ElementRef | undefined;
+  @ViewChild('postUrlElement') postUrlElement: ElementRef | undefined;
 
   postTitle!: string;
   postContent!: string;
   imageUrl!: string;
   postUrl!: string;
   addBackground!: boolean;
+
+  onSubmit() {
+
+
+      console.log("Form submitted!");
+      console.log("Post Title:", this.postTitle);
+      console.log("Post Content:", this.postContent);
+      console.log("Image URL:", this.imageUrl);
+      console.log("Post URL:", this.postUrl);
+      console.log("Add Background:", this.addBackground);
+
+    /*document.getElementById("post-title")!.innerHTML = this.postTitle;
+    document.getElementById("post-content")!.innerHTML = this.postContent;
+    document.getElementById("post-image")!.setAttribute("src", this.imageUrl);
+    document.getElementById("post-link")!.setAttribute("href", this.postUrl);*/
+
+    // @ts-ignore
+    this.postTitleElement.nativeElement.textContent = this.postTitle;
+    // @ts-ignore
+    this.postContentElement.nativeElement.textContent = this.postContent;
+    // @ts-ignore
+    this.imageUrlElement.nativeElement.setAttribute('src', this.imageUrl);
+    // @ts-ignore
+    this.postUrlElement.nativeElement.setAttribute('href', this.postUrl);
+
+    this.postTitle = '';
+    this.postContent = '';
+    this.imageUrl = '';
+    this.postUrl = '';
+    this.addBackground = false;
+
+
+  }
 }
