3f1d16068c9ad6519883a911b06f415025fb1844
DushanMalind <dushanmalinda010@gmail.com>
2024-03-06 08:02:56 +0530

    home update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {Input} from "postcss";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,63 @@ import {RouterLink} from "@angular/router";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent /*implements OnChanges*/{
+  /** Emitting event data **/
+ /* @Output() dataEmitter: EventEmitter<any> = new EventEmitter();
 
+  emitData() {
+    const data = 'Some data to emit';
+    this.dataEmitter.emit(data);
+  }*/
+
+ /* message: string = 'Initial message';
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }*/
+
+  // @ts-ignore
+/*  @ Input() name: string = '';
+  ngOnChanges(changes: SimpleChanges) {
+    for (const inputName in changes) {
+      const inputValues = changes[inputName];
+      console.log(`Previous ${inputName} == ${inputValues.previousValue}`);
+      console.log(`Current ${inputName} == ${inputValues.currentValue}`);
+      console.log(`Is first ${inputName} change == ${inputValues.firstChange}`);
+    }
+  }*/
 }
+
+
