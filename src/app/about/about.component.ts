2d1647202dbd330d9a7e5a68545bafc6915c81da
DushanMalind <dushanmalinda010@gmail.com>
2024-03-06 17:13:29 +0530

    ngModel Add input fileds

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
 
+  postTitle!: string;
+  postContent!: string;
+  imageUrl!: string;
+  postUrl!: string;
+  addBackground!: boolean;
 }
