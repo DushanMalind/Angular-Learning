4c15544090034e4eb02a4fa1cda7ee1f93d3ee01
DushanMalind <dushanmalinda010@gmail.com>
2024-03-07 15:17:49 +0530

    Fetch Object Array

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-event',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    JsonPipe
   ],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
@@ -14,6 +15,17 @@ export class EventComponent {
 
   /*NgFor Directive*/
   postArray:Array<string>=['Post 1','Post 2','Post 3','Post 4','Post 5'];
+
+  /*Fetch Object Array*/
+
+  objArray:Array<object>=[
+    {id:1,name:'Obj Post 1'},
+    {id:2,name:'Obj Post 2'},
+    {id:3,name:'Obj Post 3'},
+    {id:4,name:'Obj Post 4'},
+    {id:5,name:'Obj Post 5'}
+  ];
+
 
   constructor() {
     for (let i = 0; i < this.postArray.length; i++) {
