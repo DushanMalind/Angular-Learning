f824ca0b360ff14daf914e08a3e6fa3d6691bf64
DushanMalind <dushanmalinda010@gmail.com>
2024-03-08 21:19:17 +0530

    userArray from done

--- src/app/from/from.component.ts
+++ src/app/from/from.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-from',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './from.component.html',
   styleUrl: './from.component.css'
@@ -25,8 +28,19 @@ export class FromComponent {
       "name": this.name,
       "email": this.email,
       "feedback": this.feedback
+     /*  name: this.name,
+       email: this.email,
+      feedback: this.feedback*/
     });
     console.log(this.userArray);
+  }
+
+  onDelete(index: number){
+    /*this.userArray.pop();*/
+    this.userArray.splice(index, 1);
+
+
+
   }
 
 }
