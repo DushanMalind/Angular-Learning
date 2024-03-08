fe57789a70c2a917d812df21b78ae82ac7cb3ea7
DushanMalind <dushanmalinda010@gmail.com>
2024-03-08 17:37:12 +0530

    From Components Add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {AboutComponent} from "./about/about.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {PostComponent} from "./post/post.component";
 import {EventComponent} from "./event/event.component";
+import {FromComponent} from "./from/from.component";
 
 export const routes: Routes = [
   { path: 'home', 'title': "Home", component: HomeComponent },
   { path: 'about', 'title': "About", component: AboutComponent },
   { path: 'contact', 'title': "contact", component: PostComponent },
-  { path: 'event', 'title': "contact", component: EventComponent },
+  { path: 'event', 'title': "Event", component: EventComponent },
+  { path: 'from', 'title': "From", component: FromComponent },
   { path: '', redirectTo: "/home", pathMatch: "full" },
   {path: '**', component: PageNotFoundComponent}
 ];
