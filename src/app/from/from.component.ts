714965282974a014f7e43db6d64afb85d3beebb4
DushanMalind <dushanmalinda010@gmail.com>
2024-03-08 18:42:08 +0530

    userArray data

--- src/app/from/from.component.ts
+++ src/app/from/from.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-from',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './from.component.html',
   styleUrl: './from.component.css'
 })
 export class FromComponent {
+  name!: string;
+  email!: string;
+  feedback!: string;
+
+  userArray: Array<any> = [];
+
+  constructor() {
+  }
+
+  onClick(){
+    this.userArray.push({
+      "name": this.name,
+      "email": this.email,
+      "feedback": this.feedback
+    });
+    console.log(this.userArray);
+  }
 
 }
