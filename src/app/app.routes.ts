3c9ccfdf35fc71189b4b64bc7b313c4168a788af
DushanMalind <dushanmalinda010@gmail.com>
2024-03-10 14:21:58 +0530

    SliderNave Success Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import {PostComponent} from "./post/post.component";
 import {EventComponent} from "./event/event.component";
 import {FromComponent} from "./from/from.component";
 import {SliderNavbarComponent} from "./slider-navbar/slider-navbar.component";
+import {DashboardComponent} from "./sliderNavComponent/dashboard/dashboard.component";
+import {KanbanComponent} from "./sliderNavComponent/kanban/kanban.component";
 
 export const routes: Routes = [
   { path: 'home', 'title': "Home", component: HomeComponent },
@@ -13,7 +15,12 @@ export const routes: Routes = [
   { path: 'contact', 'title': "contact", component: PostComponent },
   { path: 'event', 'title': "Event", component: EventComponent },
   { path: 'from', 'title': "From", component: FromComponent },
-  { path: 'slider', 'title': "From", component: SliderNavbarComponent },
+  { path: 'slider', 'title': "From", component: SliderNavbarComponent,
+    children: [
+      {path: 'dashboard', data: {title: 'Dashboard'}, component: DashboardComponent},
+      {path: 'kanban', data: {title: 'Dashboard'}, component: KanbanComponent},
+    ]},
   { path: '', redirectTo: "/home", pathMatch: "full" },
   {path: '**', component: PageNotFoundComponent}
 ];
+
