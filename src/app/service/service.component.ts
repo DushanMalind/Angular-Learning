69611b693deefc6bfbf655f06424096eaa8f4149
DushanMalind <dushanmalinda010@gmail.com>
2024-03-11 15:35:20 +0530

    ServiceComponent PostAdd

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { PostService } from '../services/post.service'
+import {NgForOf} from "@angular/common";
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css'
 })
-export class ServiceComponent {
+export class ServiceComponent implements OnInit{
+
+  posts:Array<any>;
+
+  constructor() {
+    let postService = new PostService();
+    this.posts=postService.postsList;
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,26 @@
+
+export class PostService{
+
+  postsList:Array<any>=[
+{
+      title: "Post 1",
+      content: "This is the first post"
+    },
+    {
+      title: "Post 2",
+      content: "This is the second post"
+    },
+
+    {
+      title: "Post 3",
+      content: "This is the third post"
+    },
+    {
+      title: "Post 4",
+      content: "This is the fourth post"
+    }
+
+  ];
+  constructor() { }
+
+}
