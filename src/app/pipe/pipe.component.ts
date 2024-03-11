d5858af877489b27a36ef1ef2853bb282bd46076
DushanMalind <dushanmalinda010@gmail.com>
2024-03-11 11:23:15 +0530

    LowerCase Number Pipe Decimal value check pipe

--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import {UpperCasePipe} from "@angular/common";
+import {DecimalPipe, LowerCasePipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
   imports: [
-    UpperCasePipe
+    UpperCasePipe,
+    LowerCasePipe,
+    DecimalPipe
   ],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
   title:string = 'Angular Course';
+
+  count: number=2585464;
+
+  dcValue: number=2.1254;
 
   constructor() {
   }
