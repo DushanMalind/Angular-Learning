f8982ab184df52d15a8c1f2e33f095d2c129b52e
DushanMalind <dushanmalinda010@gmail.com>
2024-03-11 17:36:54 +0530

    from onSubmit

--- src/app/from/from.component.ts
+++ src/app/from/from.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, NgForm} from "@angular/forms";
 import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
@@ -44,5 +44,12 @@ export class FromComponent {
   /*if(){
     window.location.href = "https://www.google.com";
   }*/
+
+
+/*ngForm*/
+
+  onSubmit(f: NgForm){
+
+  }
 
 }
