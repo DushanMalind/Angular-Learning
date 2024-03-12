4b85f5526d5a4e2edaf2a3d0fc6ea9b10e4d6e0a
DushanMalind <dushanmalinda010@gmail.com>
2024-03-12 15:34:01 +0530

    PostLoad Components Add

---
+++ src/app/lazy-loading/postLoard/post-loard/post-loard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-loard',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-loard.component.html',
+  styleUrl: './post-loard.component.css'
+})
+export class PostLoardComponent {
+
+}
